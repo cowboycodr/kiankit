5c69105b2e300373b555bb18e4be73b3b44ebb6b
cowboycodr <kian.p.m.007@gmail.com>
2024-05-02 10:56:23 -0500

    update build target

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {

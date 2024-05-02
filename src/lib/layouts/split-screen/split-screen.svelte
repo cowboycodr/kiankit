68b65b249aeae7efb91dcd829469c8123591781d
cowboycodr <kian.p.m.007@gmail.com>
2024-05-02 12:34:42 -0500

    update demo page and split-screen layout

--- src/lib/layouts/split-screen/split-screen.svelte
+++ src/lib/layouts/split-screen/split-screen.svelte
@@ -1,11 +1,14 @@
 <div class="flex min-h-screen">
-	<div class="flex w-full items-center justify-center p-2">
+	<div class="hidden w-full items-center justify-center p-3 md:flex">
 		<div class="w-full max-w-96">
 			<slot name="left" />
 		</div>
 	</div>
-	<div class="flex w-full items-center justify-center bg-accent p-2">
-		<div class="w-full max-w-96">
+	<div class="flex w-full items-center justify-center bg-accent p-3">
+		<div class="w-full max-w-96 space-y-3">
+			<div class="flex justify-center text-center md:hidden">
+				<slot name="left" />
+			</div>
 			<slot name="right" />
 		</div>
 	</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,48 @@
 <script>
-    import { SplitScreen } from '$layouts';
+	import { SplitScreen } from '$layouts';
 
-    import Fa from 'svelte-fa';
-    import { faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons';
+	import Fa from 'svelte-fa';
+	import { faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons';
 
-    import { Button } from '$ui/button';
-    import * as Card from '$ui/card';
+	import { Button } from '$ui/button';
+	import * as Card from '$ui/card';
 </script>
 
 <SplitScreen>
-    <div slot="left">
-        <h1 class="text-3xl font-semibold">
-            KianKit
-        </h1>
-        <p class="text-muted-foreground">
-            Rapidly develop SvelteKit apps with Supabase.
-        </p>
-    </div>
-    <div slot="right">
-        <Card.Root>
-            <Card.Header>
-                <Card.Title>
-                    Sign in
-                </Card.Title>
-                <Card.Description>
-                    This will only take a few moments.
-                </Card.Description>
-            </Card.Header>
-            <Card.Content>
-                <div class="space-y-2">
-                    <Button variant="secondary" class="w-full space-x-1">
-                        <span>
-                            <Fa icon={faGoogle} />
-                        </span>
-                        <span>Google</span>
-                    </Button>
-                    <Button variant="secondary" class="w-full space-x-1" disabled>
-                        <span>
-                            <Fa icon={faGithub} />
-                        </span>
-                        <span>Github</span>
-                    </Button>
-                </div>
-            </Card.Content>
-            <Card.Footer>
-                <Card.Description>
-                    More ways to sign in coming soon.
-                </Card.Description>
-            </Card.Footer>
-        </Card.Root>
-    </div>
-    <div class="flex flex-col space-y-3" slot="description">
-        <p class="text-muted-foreground text-sm">
-            This is a demo page for <a class="underline" href="https://github.com/cowboycodr/KianKit">KianKit</a>.
-        </p>
-        <div class="flex justify-center space-x-3 text-muted-foreground">
-            <a href="https://github.com/cowboycodr/KianKit">
-                <Fa icon={faGithub} />
-            </a>
-        </div>
-    </div>
+	<div slot="left">
+			<h1 class="text-3xl font-semibold">KianKit</h1>
+			<p class="text-muted-foreground">Rapidly build SvelteKit + Supabase apps.</p>
+	</div>
+	<div slot="right">
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Sign in</Card.Title>
+				<Card.Description>This will only take a few moments.</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<div class="space-y-2">
+					<Button variant="secondary" class="w-full space-x-1">
+						<span>
+							<Fa icon={faGoogle} />
+						</span>
+						<span>Google</span>
+					</Button>
+					<Button variant="secondary" class="w-full space-x-1" disabled>
+						<span>
+							<Fa icon={faGithub} />
+						</span>
+						<span>Github</span>
+					</Button>
+				</div>
+			</Card.Content>
+			<Card.Footer>
+				<Card.Description>More ways to sign in coming soon.</Card.Description>
+			</Card.Footer>
+		</Card.Root>
+	</div>
+	<div slot="description">
+		<Card.Description>
+			This is a <a class="underline" href="https://github.com/cowboycodr/KianKit">KianKit</a> demo page.
+		</Card.Description>
+	</div>
 </SplitScreen>

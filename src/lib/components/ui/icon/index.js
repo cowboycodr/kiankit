642550e8c4265bdec9a4f69d0e19312954970936
cowboycodr <kian.p.m.007@gmail.com>
2024-05-11 22:55:44 -0500

    update icon

--- src/lib/components/ui/icon/index.js
+++ src/lib/components/ui/icon/index.js
@@ -5,7 +5,7 @@ const iconVariants = tv({
 	base: 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium',
 	variants: {
 		size: {
-			default: 'h-8 w-8',
+			default: 'h-6 w-6',
 			lg: 'h-16 w-16',
 			xl: 'h-24 w-24'
 		}

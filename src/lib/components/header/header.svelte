c1c84efd31dad3c0113e0c37f952a1fc03d74103
cowboycodr <kian.p.m.007@gmail.com>
2024-05-13 14:10:12 -0500

    update: header

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -1,4 +1,4 @@
-<header class="sticky left-0 right-0 top-0 border-b border-accent">
+<header class="sticky left-0 right-0 top-0 z-50 border-b border-accent bg-background">
 	<nav class="container w-full p-2">
 		<slot />
 	</nav>

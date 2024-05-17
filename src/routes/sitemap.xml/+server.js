6c7b4550eadf911946a98276721b3cf7dec15e62
Kian McKenna <kian.p.m.007@gmail.com>
2024-05-17 10:52:48 -0500

    sitemap now includes dynamic lastmod

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -2,13 +2,18 @@ import { text } from '@sveltejs/kit';
 
 export const GET = async (event) => {
 	const { url } = event;
+    const currentDate = new Date().toISOString().split('T')[0];
 
 	const sitemap = `
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>${url.origin}</loc>
-        <lastmod>2024-05-13</lastmod>
+        <lastmod>${currentDate}</lastmod>
+        <loc>${url.origin}/auth/login</loc>
+        <lastmod>${currentDate}</lastmod>
+        <loc>${url.origin}/auth/signup</loc>
+        <lastmod>${currentDate}</lastmod>
     </url>
 </urlset>
     `.trim();

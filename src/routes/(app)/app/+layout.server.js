e0c3ce6704d91378450df78dd3a9e636a35ea207
Kian McKenna <kian.p.m.007@gmail.com>
2024-05-22 18:41:08 -0500

    add marketing group, repurpose app group

---
+++ src/routes/(app)/app/+layout.server.js
@@ -0,0 +1,11 @@
+import { redirect } from '@sveltejs/kit';
+
+export const load = async (event) => {
+    const { parent } = event;
+
+    const { session, user } = await parent();
+
+    if (!session) {
+        throw redirect(303, '/');
+    }
+}

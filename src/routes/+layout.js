ceef8684bdf6143109220f5e16142fcdb827cd2f
cowboycodr <kian.p.m.007@gmail.com>
2024-05-28 12:36:12 -0500

    add svelte-meta-tags

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -2,7 +2,7 @@ import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/publi
 import { createBrowserClient, isBrowser, parse } from '@supabase/ssr';
 
 export const load = async (event) => {
-	const { fetch, data, depends } = event;
+	const { fetch, data, depends, url } = event;
 
 	depends('supabase:auth');
 
@@ -31,5 +31,38 @@ export const load = async (event) => {
 		data: { session }
 	} = await supabase.auth.getSession();
 
-	return { supabase, session };
+	const baseMetaTags = Object.freeze({
+		title: 'KianKit',
+		titleTemplate: '%s | KianKit',
+		description: 'Rapidly build SvelteKit + Supabase apps.',
+		canonical: new URL(url.pathname, url.origin).href,
+		openGraph: {
+			type: 'website',
+			url: new URL(url.pathname, url.origin).href,
+			locale: 'en_US',
+			title: 'KianKit',
+			description: 'Rapidly build SvelteKit + Supabase apps.',
+			siteName: 'KianKit',
+			images: [
+				{
+					url: 'https://pub-0ad6faf4526b463d9367cf8b6e642b7c.r2.dev/kiankit-og.png',
+					alt: 'KianKit',
+					width: 1200,
+					height: 630,
+					type: 'image/png'
+				}
+			]
+		},
+		twitter: {
+			handle: '@fromkian',
+			site: '@fromkian',
+			cardType: 'summary_large_image',
+			title: 'KianKit',
+			description: 'Rapidly build SvelteKit + Supabase apps.',
+			image: 'https://pub-0ad6faf4526b463d9367cf8b6e642b7c.r2.dev/kiankit-og.png',
+			imageAlt: 'Rapidly build SvelteKit + Supabase apps.'
+		}
+	});
+
+	return { supabase, session, baseMetaTags };
 };

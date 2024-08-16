f4870998ac2a18ed53b417518da76753087af366
Kian McKenna <kian@mckenna.cloud>
2024-08-15 21:53:00 -0500

    update navbar

--- src/routes/(marketing)/(navbar)/brand.svelte
+++ src/routes/(marketing)/(navbar)/brand.svelte
@@ -4,7 +4,7 @@
 
 <a href="/" class="contents">
 	<div class="flex items-center space-x-1">
-		<Logo class="h-9 w-9" />
-		<h1 class="text-xl font-medium">KianKit</h1>
+		<Logo class="h-8 w-8 md:h-9 md:w-9" />
+		<!-- <h1 class="text-xl font-medium">KianKit</h1> -->
 	</div>
 </a>
--- src/routes/(marketing)/(navbar)/menu.svelte
+++ src/routes/(marketing)/(navbar)/menu.svelte
@@ -1,10 +1,11 @@
 <script>
 	import { page } from '$app/stores';
 
-    import Menu from 'lucide-svelte/icons/menu';
+	import Menu from 'lucide-svelte/icons/menu';
+	import ChevronDown from 'lucide-svelte/icons/chevron-down';
 
 	import { Button } from '$ui/button';
-    import * as Sheet from '$ui/sheet';
+	import * as Sheet from '$ui/sheet';
 
 	import AuthButtons from './auth-buttons.svelte';
 	import ThemeToggle from './theme-toggle.svelte';
@@ -24,51 +25,55 @@
 	>
 		Github
 	</Button>
-	<Button size="sm" variant="ghost" class="rounded-full">Documentation</Button>
+	<Button size="sm" variant="ghost" class="rounded-full">Docs</Button>
 </div>
 <div class="flex w-full items-center justify-end space-x-1">
-	<ThemeToggle />
 	<div class="hidden md:block">
+		<ThemeToggle />
 		{#if session}
 			<UserDropdown {session} />
 		{:else}
 			<AuthButtons />
 		{/if}
 	</div>
-    <div class="md:hidden">
-        <Sheet.Root open>
-            <Sheet.Trigger asChild let:builder>
-                <Button builders={[builder]} size="icon-sm" variant="ghost">
-                    <Menu />
-                </Button>
-            </Sheet.Trigger>
-            <Sheet.Content class="flex flex-col h-full">
-                <Sheet.Header>
-                    <Sheet.Title>
-                        Menu
-                    </Sheet.Title>
-                </Sheet.Header>
-                <div>
-                    <div>
-                        {#if session}
-                        <form method="POST" action="/auth?/signout" class="contents">
-                            <Button variant="outline" type="submit">
-                                Sign out
-                            </Button>
-                        </form>
-                    {:else}
-                        <div class="w-full space-y-1">
-                            <Button href="/auth/login" class="w-full" size="sm">
-                                Sign up
-                            </Button>
-                            <Button href="/auth/login" variant="outline" class="w-full" size="sm">
-                                Log in
-                            </Button>
-                        </div>
-                    {/if}
-                    </div>
-                </div>
-            </Sheet.Content>
-        </Sheet.Root>
-    </div>
+	<div class="md:hidden">
+		<Sheet.Root open>
+			<Sheet.Trigger asChild let:builder>
+				<Button builders={[builder]} size="icon-sm" variant="ghost">
+					<Menu />
+				</Button>
+			</Sheet.Trigger>
+			<Sheet.Content class="flex h-full flex-col">
+				<Sheet.Header>
+					<Sheet.Title>Menu</Sheet.Title>
+				</Sheet.Header>
+				<div class="flex-grow space-y-5">
+					<div>
+						{#if session}
+							<form method="POST" action="/auth?/signout" class="contents">
+								<Button variant="outline" type="submit">Sign out</Button>
+							</form>
+						{:else}
+							<div class="w-full space-y-1">
+								<Button href="/auth/login" class="w-full" size="sm">Sign up</Button>
+								<Button href="/auth/login" variant="outline" class="w-full" size="sm">
+									Log in
+								</Button>
+							</div>
+						{/if}
+					</div>
+					<div class="text-secondary-foreground">
+						<Button variant="ghost" class="w-full justify-between text-base">Github</Button>
+						<Button variant="ghost" class="w-full justify-between text-base">
+							Docs
+							<ChevronDown class="h-4" />
+						</Button>
+					</div>
+				</div>
+				<Sheet.Footer class="w-full flex-row justify-end">
+					<ThemeToggle />
+				</Sheet.Footer>
+			</Sheet.Content>
+		</Sheet.Root>
+	</div>
 </div>

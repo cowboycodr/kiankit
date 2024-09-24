7ac5eab33d396d11dcb2e7774b99b0cd603e2030
Kian McKenna <kian.p.m.007@gmail.com>
2024-09-24 16:12:17 -0500

    use node adapter by default

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {

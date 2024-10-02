89adfba55ae1c425f041a1be999aff07e65d2071
Kian McKenna <kian.p.m.007@gmail.com>
2024-10-02 17:37:24 -0500

    add shadcn components

---
+++ src/lib/components/ui/card/card-header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn } from '@/utils.js';
+
+	type $$Props = HTMLAttributes<HTMLDivElement>;
+
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+</script>
+
+<div class={cn('flex flex-col space-y-1.5 p-6 pb-0', className)} {...$$restProps}>
+	<slot />
+</div>

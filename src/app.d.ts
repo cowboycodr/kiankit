15685ec859885af8dcda3ec93ce3c2a6a6a39c3c
Kian McKenna <kian.p.m.007@gmail.com>
2024-10-02 18:19:58 -0500

    add supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,20 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { Session, SupabaseClient, User } from '@supabase/supabase-js';
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			supabase: SupabaseClient;
+			safeGetSession: () => Promise<{ session: Session | null; user: User | null }>;
+			session: Session | null;
+			user: User | null;
+		}
+		interface PageData {
+			session: Session | null;
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
-<script lang="ts">
+<script>
 	import '../app.pcss';
+
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	export let data;
+	$: ({ session, supabase } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <slot />

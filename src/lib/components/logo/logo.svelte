28e2ff1995bb7d9dd51710ae8d4cb9daf4fc84e1
Kian McKenna <kian.p.m.007@gmail.com>
2024-10-02 18:06:52 -0500

    add logo component

---
+++ src/lib/components/logo/logo.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { cn } from '@/utils';
+
+	import { tv, type VariantProps } from 'tailwind-variants';
+	import Logo from 'lucide-svelte/icons/hexagon';
+
+	export const variants = tv({
+		base: 'fill-primary text-primary',
+		variants: {
+			size: {
+				md: 'h-8 h-8',
+				sm: 'h-7 w-7'
+			}
+		},
+		defaultVariants: {
+			size: 'md'
+		}
+	});
+
+	let className: string | undefined = '';
+	export { className as class };
+	export let size: VariantProps<typeof variants>['size'] = 'md';
+</script>
+
+<Logo class={cn(variants({ size }), className)} />

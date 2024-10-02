75bdcc2350068dad3e458adcdc002cb7fd14b42e
Kian McKenna <kian.p.m.007@gmail.com>
2024-10-02 17:31:36 -0500

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,9 @@ export default tseslint.config(
 		}
 	},
 	{
-		ignores: ['build/', '.svelte-kit/', 'dist/']
+		ignores: ['build/', '.svelte-kit/', 'dist/'],
+		rules: {
+			'@typescript-eslint/no-require-imports': 'off'
+		}
 	}
 );

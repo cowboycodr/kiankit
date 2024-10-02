078ba888aeec8a1ebfdef4133c2cc4d5c74dc9ed
Kian McKenna <kian.p.m.007@gmail.com>
2024-10-02 18:32:09 -0500

    update default styles

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -8,8 +8,9 @@ const buttonVariants = tv({
 		variant: {
 			default: 'bg-primary text-primary-foreground hover:bg-primary/90',
 			destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-			outline: 'border-input bg-background hover:bg-accent hover:text-accent-foreground border',
-			secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+			outline:
+				'border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground border',
+			secondary: 'bg-secondary border text-secondary-foreground hover:bg-secondary/80',
 			ghost: 'hover:bg-accent hover:text-accent-foreground',
 			link: 'text-primary underline-offset-4 hover:underline'
 		},

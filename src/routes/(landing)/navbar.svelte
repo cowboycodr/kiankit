143cb41e20c777afb385853dfc3e3f47b0070ab2
Kian McKenna <kian.p.m.007@gmail.com>
2024-10-03 16:48:21 -0500

    update mobile menu logout

--- src/routes/(landing)/navbar.svelte
+++ src/routes/(landing)/navbar.svelte
@@ -83,10 +83,12 @@
 					<div class="flex-1 space-y-5">
 						<div class="space-y-3">
 							{#if session}
-								<Button class="w-full" variant="outline">
-									<LogOutIcon size="16" />
-									<span> Log out </span>
-								</Button>
+								<form action="/auth?/logout" method="POST" class="contents" use:enhance>
+									<Button class="w-full space-x-1" variant="outline" type="submit">
+										<LogOutIcon size="16" />
+										<span> Log out </span>
+									</Button>
+								</form>
 							{:else}
 								<Button class="w-full" variant="outline" href="/login">Log in</Button>
 								<Button class="w-full" href="/signup">Sign up</Button>

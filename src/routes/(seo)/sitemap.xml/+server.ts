5d79001980efadaa320eb90e07a69c8a006361ce
Kian McKenna <kian.p.m.007@gmail.com>
2024-10-07 11:47:34 -0500

    update sitemap

--- src/routes/(seo)/sitemap.xml/+server.ts
+++ src/routes/(seo)/sitemap.xml/+server.ts
@@ -1,12 +1,18 @@
 import * as sitemap from 'super-sitemap';
 import type { RequestHandler } from '@sveltejs/kit';
+import type { Post } from '../../(landing)/types';
 
-export const GET: RequestHandler = async ({ url }) => {
+export const GET: RequestHandler = async ({ url, fetch }) => {
+	const postsResponse = await fetch('/data/posts/posts.json');
+	const posts: Post[] = await postsResponse.json();
+	const postSlugs = posts.map((a) => a.slug);
+
 	return await sitemap.response({
 		origin: url.origin,
 		excludeRoutePatterns: ['.*\\(app\\).*'],
 		paramValues: {
-			'/[method=auth_method]': ['login', 'signup']
+			'/[method=auth_method]': ['login', 'signup'],
+			'/article/[slug]': postSlugs
 		}
 	});
 };

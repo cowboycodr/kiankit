bc50dc16a38a0ff7038c352aaf8b6d82db4d842b
cowboycodr <kian.p.m.007@gmail.com>
2024-10-06 18:02:36 -0700

    add posts viewer

--- src/routes/(landing)/blog/+page.svelte
+++ src/routes/(landing)/blog/+page.svelte
@@ -6,16 +6,21 @@
 </script>
 
 <div class="container max-w-[1024px] py-12">
-	<div class="grid auto-rows-fr">
+	<div class="grid auto-rows-fr grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
 		{#each posts as post}
-			<Card.Root>
-				<Card.Header>
-					<Card.Title>
-						{post.title}
-					</Card.Title>
-				</Card.Header>
-				<Card.Content />
-			</Card.Root>
+			<a href={`/blog/${post.slug}`}>
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>
+							{post.title}
+						</Card.Title>
+						<Card.Description>
+							{post.description}
+						</Card.Description>
+					</Card.Header>
+					<Card.Content />
+				</Card.Root>
+			</a>
 		{/each}
 	</div>
 </div>
